614de16be93eb51fba544a970c5b5083f2c2d218
Nick McMillan <nick@myrytebytes.com>
2013-12-09 18:18:02 -0600

    Model Updates
    - Model updates
    - Layout updates for order summary table.

---
+++ RyteBytes/BeginLoginNavViewController.h
@@ -0,0 +1,13 @@
+//
+//  BeginLoginNavViewController.h
+//  RyteBytes
+//
+//  Created by Nicholas McMillan on 12/6/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BeginLoginNavViewController : UINavigationController
+
+@end
---
+++ RyteBytes/BeginLoginNavViewController.m
@@ -0,0 +1,38 @@
+//
+//  BeginLoginNavViewController.m
+//  RyteBytes
+//
+//  Created by Nicholas McMillan on 12/6/13.
+//
+//
+
+#import "BeginLoginNavViewController.h"
+
+@interface BeginLoginNavViewController ()
+
+@end
+
+@implementation BeginLoginNavViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
