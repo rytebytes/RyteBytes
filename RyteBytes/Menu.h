df9dfbf9902f993202d73981462c2545ea34ef5f
Nick McMillan <nick@myrytebytes.com>
2013-12-16 17:05:27 -0600

    Writing menu to disk
    - Added plist for menu
    - Check if user is logged in on MenuItemDetails VC
    - Added new menu model object.

---
+++ RyteBytes/Menu.h
@@ -0,0 +1,13 @@
+//
+//  Menu.h
+//  RyteBytes
+//
+//  Created by Nicholas McMillan on 12/11/13.
+//
+//
+
+#import "JSONModel.h"
+
+@interface Menu : JSONModel
+
+@end
---
+++ RyteBytes/Menu.m
@@ -0,0 +1,13 @@
+//
+//  Menu.m
+//  RyteBytes
+//
+//  Created by Nicholas McMillan on 12/11/13.
+//
+//
+
+#import "Menu.h"
+
+@implementation Menu
+
+@end
