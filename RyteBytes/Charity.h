144d19532183e2f9125f70bddd54f7df505c0898
Nick McMillan <nick@myrytebytes.com>
2013-12-19 16:05:19 -0600

    Model updates
    - finished splitting menu into its own class
    - added charity model
    - charity object now returned with location object
    -

---
+++ RyteBytes/Charity.h
@@ -0,0 +1,13 @@
+//
+//  Charity.h
+//  RyteBytes
+//
+//  Created by Nicholas McMillan on 12/16/13.
+//
+//
+
+#import "JSONModel.h"
+
+@interface Charity : JSONModel
+
+@end
---
+++ RyteBytes/Charity.m
@@ -0,0 +1,13 @@
+//
+//  Charity.m
+//  RyteBytes
+//
+//  Created by Nicholas McMillan on 12/16/13.
+//
+//
+
+#import "Charity.h"
+
+@implementation Charity
+
+@end
